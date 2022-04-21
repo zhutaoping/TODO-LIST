2db78912df32ffdde06507557ed44dc41ccd40ee
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-04-22 01:52:26 +0800

    set up complete revisited

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/js/app.js",
+	entry: "./src/js/controller.js",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
